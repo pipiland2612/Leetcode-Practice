bd681176331db5ca5cf8427d1fa9e15e24df7dcb
pipiland2612 <nguyen.t.dang.minh@gmail.com>
2024-12-17 20:50:50 +0200

    Solve coin change

--- DynamicProgramming/src/DynamicProgrammingProblem.java
+++ DynamicProgramming/src/DynamicProgrammingProblem.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class DynamicProgrammingProblem {
     // Climb stairs Problem
     public static int climbStairs(int n) {
@@ -109,10 +111,23 @@ public class DynamicProgrammingProblem {
 
         return count1[n - 1] + count2[n - 1];
     }
+
+    // Coin change problem
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i < coin) continue;
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        return dp[amount] != amount + 1 ? dp[amount] : -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(numDecodings("226"));
-        System.out.println(numDecodings("909"));
-        System.out.println(numDecodings("20419"));
+        System.out.println(coinChange(new int[]{1,2,5},11));
     }
 
 }
